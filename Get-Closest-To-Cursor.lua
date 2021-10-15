89863d48a3117e013af2d0cd384560c1a9f45ae2
yesok <70432888+yesok3877@users.noreply.github.com>
2021-10-15 12:57:43 -0500

    Update Get-Closest-To-Cursor.lua

--- Get-Closest-To-Cursor.lua
+++ Get-Closest-To-Cursor.lua
@@ -1,31 +1,21 @@
-local Players = game:GetService("Players")
+local Players = game.Players
 local LocalPlayer = Players.LocalPlayer
 local Mouse = LocalPlayer:GetMouse()
 local Camera = workspace.CurrentCamera
 
 return function()
-	local closestTarg = math.huge
-	local Target = nil
-	for _, Player in next, Players:GetPlayers() do
-        if Player ~= LocalPlayer then
-            local playerCharacter = Player.Character
-            if playerCharacter then
-                local playerHumanoid = playerCharacter:FindFirstChild("HumanoidRootPart")
-                if playerHumanoid then
-                    local hitVector, onScreen = Camera:WorldToScreenPoint(playerHumanoid.Position)
-                    if onScreen then
-                        local CCF = Camera.CFrame.p
-                        if workspace:FindPartOnRayWithIgnoreList(Ray.new(CCF, (playerHumanoid.Position-CCF).unit * 5000),{Player}) then
-                            local hitTargMagnitude = (Vector2.new(Mouse.X, Mouse.Y) - Vector2.new(hitVector.X, hitVector.Y)).magnitude
-                            if hitTargMagnitude < closestTarg then
-                                Target = Player
-                                closestTarg = hitTargMagnitude
-                            end
-                        end
-                    end
+    local Range, Target = math.huge
+    for _, Player in pairs(Players:GetPlayers()) do
+        if Player ~= Players.LocalPlayer and Player.Character and Player.Character.PrimaryPart then
+            local Position, Visible = Camera:WorldToScreenPoint(Player.Character.PrimaryPart.Position)
+            if Visible then
+                local Distance = Vector2.new((Position.X - Mouse.X), (Position.Y - Mouse.Y)).magnitude
+                if Distance <= Range then
+                    Range = Distance 
+                    Target = Player
                 end
             end
-		end
-	end
-	return Target
+        end
+    end
+    return Target
 end
