85cda944a45c5695126f0cde758be6f69c6dcc9c
yesok <70432888+yesok3877@users.noreply.github.com>
2021-12-02 16:21:57 -0600

    Update Get-Closest-To-Cursor.lua

--- Get-Closest-To-Cursor.lua
+++ Get-Closest-To-Cursor.lua
@@ -11,12 +11,16 @@ local Elements = table.getn
 return function()
     local Range, Target = math.huge
     local Child = Children(Players)
+    
     for Index = 1, Elements(Child) do
         local Player = rawget(Child, Index)
+        
         if Player ~= Players.LocalPlayer and Player.Character and Player.Character.PrimaryPart then
             local Position, Visible = ScreenPoint(Camera, Player.Character.PrimaryPart.Position)
+            
             if Visible then
                 local Distance = Vector2((Position.X - Mouse.X), (Position.Y - Mouse.Y)).magnitude
+                
                 if Distance <= Range then
                     Range = Distance 
                     Target = Player
@@ -24,5 +28,6 @@ return function()
             end
         end
     end
+    
     return Target
 end
